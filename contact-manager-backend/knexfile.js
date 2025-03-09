fe941accd86368c0d4701210a1cfe914c7821628
saiteja-adapa-2909 <98104890+saiteja-adapa-2909@users.noreply.github.com>
2025-03-09 13:14:38 +0530

    Update database filename

--- contact-manager-backend/knexfile.js
+++ contact-manager-backend/knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
     development: {
       client: "sqlite3",
       connection: {
-        filename: "./contacts.db",
+        filename: "./contacts.db", // i might change this later acc to my db :D
       },
       useNullAsDefault: true,
     },
